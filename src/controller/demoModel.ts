9b7d59a0ae188297e1b7b5ed076b1d48f9905bc3
AsMuin <asmuins@foxmail.com>
2025-01-02 01:52:27 +0800

    update: 导出Model的样例

--- src/controller/demoModel.ts
+++ src/controller/demoModel.ts
@@ -7,4 +7,6 @@ const demoSchema = new mongoose.Schema<IDemo>({
     name: { type: String, required: true }
 });
 
-export default mongoose.model<IDemo>('Demo', demoSchema);
+const Demo = mongoose.models.Demo || mongoose.model<IDemo>('Demo', demoSchema);
+
+export default Demo;
